5d3ad582734842ab054be36fb960e0bb83bce917
James H Wade <james.wade1221@gmail.com>
2017-11-15 12:17:10 -0500

    new layout files

--- 2_CalibrationStation.R
+++ 2_CalibrationStation.R
@@ -1,171 +1,120 @@
-CombineNetShifts <- function(){
-        library(tidyverse)
-        
-        directory <- getwd()
-        
+combineNetShifts <- function(){
         netList <- grep('net', 
                         list.files(pattern = '.csv', recursive = TRUE), 
                         value = TRUE)
         
-        removeFiles <- grep("Combined", netList, value = TRUE)
-        netList <- netList[!netList %in% removeFiles]
+        netShifts <- lapply(netList, function(i){
+                netShift <- read.csv(i)
+                # convert Target and Experiment to character type to avoid
+                # warnings when using bind_rows function below
+                netShift$Target <- as.character(netShift$Target)
+                netShift$Experiment <- as.character(netShift$Experiment)
+                netShift
+        })
         
-        netShifts <- lapply(netList, read_csv)
+        netComb <- dplyr::bind_rows(netShifts)
+        netComb <- dplyr::filter(netComb, 
+                                 !grepl("thermal|Ignore|Control", Target))
         
-        netComb <- bind_rows(netShifts)
-        netComb <- filter(netComb, !grepl("thermal|BL", Target))
+        netCombAvg <- dplyr::group_by(netComb, Target, Concentration)
+        netCombAvg <- dplyr::summarise_at(netCombAvg, 
+                                          dplyr::vars(NetShift),
+                                          dplyr::funs(mean, stats::sd))
 
-        write_csv(netComb, path = "netShiftsCombined.csv")
-        
-        setwd(directory)
+        readr::write_csv(netComb, path = "combinedNetShifts.csv")
+        readr::write_csv(netCombAvg, path = "combinedNetShifts_Avg.csv")
+        return(netComb)
 }
 
-PlotCombineNetShifts <- function(){
-        library(tidyverse)
-        library(ggthemes)
-        library(reshape2)
+plotCombineNetShifts <- function(data){
+        ggplot2::theme_set(ggthemes::theme_few(base_size = 16))
         
-        dat <- read_csv("netShiftsCombined.csv")
+        plot <- ggplot2::ggplot(data, 
+                                ggplot2::aes(x = Concentration,
+                                             y = NetShift,
+                                             color = Target,
+                                             group = Concentration)) +
+                ggplot2::ggtitle(name) + 
+                ggplot2::scale_x_log10() +
+                ggplot2::labs(x = "Analyte Concentration (pg/mL)",
+                              y = expression(paste("Relative Shift (",
+                                                   Delta,"pm)")),
+                              color = "Target")
         
-        dat.melt <- melt(dat, id = c("Step", "Target", "Ring", "Channel"), measure = "Net Shift")
-        
-        ggplot(dat.melt, aes(x = Step, y = value, color = Target)) + geom_point() + facet_wrap(~Target) + theme_few(base_size = 16)
-        
-        plot_theme <- theme_few(base_size = 16)
-        
-        plot <- ggplot(dat, aes(x = Step, y = `Net Shift`, 
-                                color = Target)) +
-                geom_point()+
-                plot_theme + ggtitle(name) + facet_wrap(~Target) +
-                scale_x_log10() +
-                labs(x = "Analyte Concentration (pg/mL)", 
-                     y = expression(paste("Relative Shift (",Delta,"pm)")),
-                     color = "Target")
-                
-        
-        plot
+        plot1 <- plot + ggplot2::geom_point()
+        plot1a <- plot1 + ggplot2::facet_wrap(~Target)
+        plot2 <- plot + ggplot2::geom_boxplot()
+        plot2a <- plot2 + ggplot2::facet_wrap(~Target)
 
-        ggsave(plot = plot, 
-               filename = paste0("netShiftsCombined_ch.png"),
-               width = 8, height = 6)
+        ggplot2::ggsave(plot = plot1,
+                        filename = paste0("netShiftsCombined_point.png"),
+                        width = 16, height = 12)
+        ggplot2::ggsave(plot = plot1a,
+                        filename = paste0("netShiftsCombined_pointwrap.png"),
+                        width = 16, height = 12)
+        ggplot2::ggsave(plot = plot2,
+                        filename = paste0("netShiftsCombined_box.png"),
+                        width = 16, height = 12)
+        ggplot2::ggsave(plot = plot2a,
+                        filename = paste0("netShiftsCombined_boxwrap.png"),
+                        width = 16, height = 12)
 }
 
-Fit <- function(loc = 'plots', tarList){
-        library(scales)
-        dat <- read_csv("netShiftsCombined.csv")
-        theme_set(theme_few(base_size = 16))
+fitCalCurves <- function(data, loc = 'plots', tarList){
+        ggplot2::theme_set(ggthemes::theme_few(base_size = 16))
         
-        dat <- filter(dat, Target %in% tarList)
-        
-        
-        dat$Replicate <- as.factor((dat$Ring - 1) %% 4 + 1)
-        
+        tarList <- unique(data$Target)
         fit <- list()
-        
-        plot.1 <- ggplot(dat, aes(x = Step, 
-                                  y = `Net Shift`, 
-                                  color = factor(Group))) + 
-                geom_point()
-                
-                
-        plot.1a <- plot.1 + scale_x_log10()
-        
-        plot.2 <- ggplot(dat, aes(x = Step, y = `Net Shift`)) +#, color = Target)) +
-                geom_point() +
-                geom_smooth(method = 'nls',
-                            formula = y ~ A + (B - A) / (1 + (x / C) ^ D),
-                            method.args = list(start = c(A = 10000,
-                                                         B = 100,
-                                                         C = 4000,
-                                                         D = 1)),
-                            se = FALSE) +
-                labs(x = "Analyte Concentration (pg/mL)",
-                     y = expression(paste("Relative Shift (",Delta,"pm)")))
-
-        plot.2a <- plot.2 + coord_trans(x = "log10")
-        
-        for(i in 1:length(tarList)) {
+        for(i in seq_len(length(tarList))) {
                 tar <- tarList[i]
-                dat.tar <- filter(dat, Target == tar)
-                y <- dat.tar$`Net Shift`
-                x <- dat.tar$Step
-                #fit[[i]] <- nls(y ~ SSlogis(x, Asym, xmid, scal))
-                fit.info <- nls(formula = y ~ A + (B - A) / (1 + (x / C) ^ D),
+                tarDat <- dplyr::filter(dat, Target == tar)
+                y <- tarDat$NetShift
+                x <- tarDat$Concentration
+                print(tar)
+                runNext <- TRUE
+                tarFit <- tryCatch({fit.info <- nls(formula = y ~ A + (B - A) / 
+                                                            (1 + (x / C) ^ D),
                                 start = list(A = 10000,
                                              B = 100,
                                              C = 4000,
-                                             D = 1),
-                                control = list(tol = 5e-08, 
-                                               minFactor = 0,
-                                               maxiter  = 10000))
-                fit[[i]] <- summary(fit.info)
-                
-                A <- as.numeric(coef(fit.info)[1])
-                B <- as.numeric(coef(fit.info)[2])
-                C <- as.numeric(coef(fit.info)[3])
-                D <- as.numeric(coef(fit.info)[4])
-                
-                testFun <- function(x) {A + (B - A) / (1 + (x / C) ^ D)}
-                
-                plot.3 <- ggplot(dat.tar, aes(x = Step, y = `Net Shift`)) +
-                        geom_boxplot(aes(group = Step), fill = "red") +
-                        stat_function(fun = testFun, color = "blue", size = 1) +
-                        scale_x_log10(breaks = 
-                                        trans_breaks("log10", function(x) 10^x),
-                                      labels = 
-                                        trans_format("log10", 
-                                                     math_format(10 ^ .x))) +
-                        labs(x = "Analyte Concentration (pg/mL)",
-                             y = expression(paste("Relative Shift (",
-                                                  Delta,
-                                                  "pm)"))) +
-                        annotation_logticks()
-                
-                ggsave(plot.3, filename = paste0("plot3_", tar, ".png"),
-                       width = 8, height = 6)
+                                             D = 1))
+                },
+                         error = function(e) {"failed"},
+                         finally = print(tar))
+                if(tarFit[1] != "failed"){
+                        fit[[i]] <- broom::tidy(fit.info)
+                        fit[[i]]$Target <- unique(tarDat$Target)
+                        A <- as.numeric(coef(fit.info)[1])
+                        B <- as.numeric(coef(fit.info)[2])
+                        C <- as.numeric(coef(fit.info)[3])
+                        D <- as.numeric(coef(fit.info)[4])
+                        
+                        testFun <- function(x) {A + (B - A) / (1 + (x / C) ^ D)}
+                        
+                        plot <- ggplot2::ggplot(tarDat,
+                                                  ggplot2::aes(x = Concentration,
+                                                               y = NetShift,
+                                                               group = Concentration)) +
+                                ggplot2::geom_boxplot(fill = "red") +
+                                ggplot2::stat_function(fun = testFun,
+                                                       color = "blue", size = 1) +
+                                ggplot2::scale_x_log10(breaks = scales::trans_breaks("log10", function(x) 10^x),
+                                                       labels = scales::trans_format("log10",
+                                                                                     scales::math_format(10 ^ .x))) +
+                                ggplot2::labs(x = "Analyte Concentration (pg/mL)",
+                                              y = expression(paste("Relative Shift (",
+                                                                   Delta,
+                                                                   "pm)"))) +
+                                ggplot2::annotation_logticks()
+                        
+                        ggplot2::ggsave(plot,
+                                        filename = paste0(tar, "CalCurve.png"),
+                                        width = 8, height = 6)
+                }
         }
+        
+        fit <- dplyr::bind_rows(fit)
         
         capture.output(fit, file = "fitInfo.txt")
-        ggsave(plot.1, filename = "plot1.png", width = 8, height = 6)
-        ggsave(plot.1a, filename = "plot1a.png", width = 8, height = 6)
-        ggsave(plot.2, filename = "plot2.png", width = 8, height = 6)
-        ggsave(plot.2a, filename = "plot2a.png", width = 8, height = 6)
-        
-}
-
-AnalyzeCalData <- function() {
-        GetName()
-        runConc <- basename(getwd())
-        ch1_IL6 <- unlist(strsplit(runConc, split = "_"))[4]
-        # ch1_IL6 <- unlist(strsplit(runConc, split = "_"))[5]
-        ch2_IL6 <- unlist(strsplit(runConc, split = "_"))[6]
-        # ch2_IL6 <- unlist(strsplit(runConc, split = "_"))[8]
-        AggData(filename = 'groupNames_allClusters.csv')
-        SubtractControl(ch = 1, cntl = "thermal")
-        SubtractControl(ch = 2, cntl = "thermal")
-        # PlotRingData(cntl = cntl, ch = 1, splitPlot = TRUE)
-        # PlotRingData(cntl = cntl, ch = 2, splitPlot = TRUE)
-        GetNetShifts(cntl = "thermal", ch = 1, 
-                     time1 = 53, time2 = 41, step = ch1_IL6)
-        # GetNetShifts(cntl = cntl, ch = 1, target = "IL-6",
-        #              time1 = 52, time2 = 41, step = ch1_IL6)
-        GetNetShifts(cntl = "thermal", ch = 2,
-                     time1 = 53, time2 = 41, step = ch2_IL6)
-        # GetNetShifts(cntl = cntl, ch = 2, target = "IL-6",
-        #                      time1 = 52, time2 = 41, step = ch2_IL6)
-        # PlotNetShifts(cntl = cntl, ch = 1, step = ch1_IL6, target = "IL-6")
-        # PlotNetShifts(cntl = cntl, ch = 2, step = ch2_IL6, target = "IL-6")
-}
-
-CalibrationStation <- function() {
-        foldersList <- list.dirs(recursive = FALSE)
-        for (i in foldersList){
-                directory <- getwd()
-                setwd(i)
-                AnalyzeData()
-                setwd(directory)
-        }
-        AnalyzeCalData()
-        PlotCombineNetShifts()
-        Fit(tarList = "IL-6")
+        readr::write_csv(fit, path = "fitInfo.csv")
 }
--- 3_PlotCompiledData.R
+++ 3_PlotCompiledData.R
@@ -1,80 +1,102 @@
 ## Plot all data combined
-PlotAllData <- function(){
-        g <- ggplot(dat, aes(x = Target, y = NormLog))
+PlotAllData <- function(data){
+        g <- ggplot(data, aes(x = Target, y = NormLog))
         
-        all.point <- g + 
-                geom_point(aes(color = factor(Replicate)),
+        allPoint <- g + 
+                geom_point(aes(color = factor(TimePoint)),
                            position = "jitter", alpha = 0.7) +
-                facet_grid(Treatment ~ interaction(TimePoint, CellLine)) + 
-                theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-                ggtitle("Full Dataset")
+                facet_grid(Treatment ~ CellLine) + 
+                theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                      legend.position = "bottom") +
+                ggtitle("Full Dataset") +
+                labs(y = "Normalized Response", color = "Time Point")
         
+        ggsave(allPoint, filename = "everything_point.png", 
+               width = 20, height = 12)
         
-        ggsave(all.point, filename = "everything_point.png", 
-               width = 20, height = 16)
-        
-        all.boxplot <- g + 
+        allBoxplot <- g + 
                 geom_boxplot(aes(fill = Treatment)) +
-                facet_grid(Treatment ~ interaction(TimePoint, CellLine)) +
-                theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+                facet_grid(CellLine~TimePoint) +
+                theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                      legend.position = "bottom") +
                 ggtitle("Full Dataset") +
                 ylab("Normalized Response")
         
-        ggsave(all.boxplot, filename = "everything_boxplot.png",
-               width = 20, height = 16)
+        ggsave(allBoxplot, filename = "everything_boxplot.png",
+               width = 20, height = 12)
+        
+        allTarget <- ggplot(data, aes(x = Treatment, y = NormLog)) +
+                geom_boxplot(aes(fill = interaction(TimePoint, CellLine))) + 
+                facet_wrap(~Target) +
+                labs(y = "Normalized Response", fill = "") +
+                theme(axis.text.x = element_text(angle = 45, hjust = 1),
+                      legend.position = "bottom")
+        
+        ggsave(allTarget, filename = "everything_targetwrap.png",
+               width = 20, height = 12)
 }
 
 ## Plot each target as Net Shift vs Treatment
-PlotEachTarget <- function(){
-        targetList <- unique(dat$Target)
+PlotEachTarget <- function(data){
+        targetList <- unique(data$Target)
         
         for(i in targetList) {
                 
-                dat.tar <- filter(dat, Target == i & Treatment != "(+)-Serum" & 
+                tarDat <- filter(data, Target == i & Treatment != "(+)-Serum" & 
                                           Treatment != "(-)-Serum")
                 
-                g <- ggplot(dat.tar, aes(x = Treatment, y = NormLog))
+                g <- ggplot(tarDat, aes(x = Treatment, y = NormLog))
                 
                 plotName <- unlist(strsplit(i, "/"))[1]
                 
-                target.point <- g + 
-                        geom_point(aes(color = Treatment), 
+                targetPoint <- g + 
+                        geom_point(aes(color = TimePoint), 
                                    position = "jitter") + 
-                        facet_grid(CellLine~TimePoint) +
+                        facet_grid(CellLine~.) +
                         ggtitle(paste0("Target: ", i)) + 
-                        ylab("Normalized Response") +
+                        labs(y = "Normalized Response", color = "") +
                         theme(axis.text.x = element_text(angle = 45, hjust = 1))
                 
-                target.box <- g + 
-                        geom_boxplot(aes(fill = Treatment)) + 
-                        facet_grid(CellLine~TimePoint) +
+                targetBox <- g + 
+                        geom_boxplot(aes(fill = TimePoint)) + 
+                        facet_grid(CellLine~.) +
                         ggtitle(paste0("Target: ", i)) + 
-                        ylab("Normalized Response") +
+                        labs(y = "Normalized Response", fill = "") +
+                        theme(axis.text.x = element_text(angle = 45, hjust = 1))
+                
+                targetBox2 <- g + 
+                        geom_boxplot(aes(fill = interaction(TimePoint, 
+                                                             CellLine))) + 
+                        ggtitle(paste0("Target: ", i)) + 
+                        labs(y = "Normalized Response", fill = "") +
                         theme(axis.text.x = element_text(angle = 45, hjust = 1))
                 
                 dir.create("Target Plots", showWarnings = FALSE)
                 
-                ggsave(target.point, 
+                ggsave(targetPoint, 
                        filename = paste0("Target Plots/", 
                                          plotName, "_point.png"), 
-                       width = 12, height = 8)
-                
-                ggsave(target.box, 
+                       width = 8, height = 6)
+                ggsave(targetBox, 
                        filename = paste0("Target Plots/", 
                                          plotName, "_boxplot.png"),
-                       width = 12, height = 8)
+                       width = 8, height = 6)
+                ggsave(targetBox2, 
+                       filename = paste0("Target Plots/", 
+                                         plotName, "_boxplot_2.png"),
+                       width = 8, height = 6)
         }
 }
 
 ## Plot each treatment as Net Shift vs Target
-PlotEachTreatment <- function(){
-        treatmentList <- unique(dat$Treatment)
+PlotEachTreatment <- function(data){
+        treatmentList <- unique(data$Treatment)
         
         for (i in treatmentList) {
                 
-                dat.rx <- filter(dat, Treatment == i)
+                rxDat <- filter(data, Treatment == i)
                 
-                g <- ggplot(dat.rx, aes(x = Target, y = NormLog))
+                g <- ggplot(rxDat, aes(x = Target, y = NormLog))
                 
                 fig4 <- g + 
                         geom_point(aes(color = Target), position = "jitter") + 
@@ -103,17 +125,17 @@ PlotEachTreatment <- function(){
 }
 
 ## Plot Treatments
-PlotTreatment <- function(control, treatment, targets, cellLine){
-        dat.cntl <- filter(dat, Treatment == control & TimePoint == "1h")
-        dat.all <- rbind(filter(dat, Treatment == treatment), dat.cntl)
-        dat.all$Treatment <- factor(dat.all$Treatment, 
+PlotTreatment <- function(data, control, treatment, targets, cellLine){
+        cntlDat <- filter(data, Treatment == control & TimePoint == "1h")
+        allDat <- rbind(filter(data, Treatment == treatment), cntlDat)
+        allDat$Treatment <- factor(allDat$Treatment, 
                                     levels = c("DMSO", "(-)-Serum", 
                                                "Apitolisib", "Erlotinib",
                                                "Palbociclib", "GNE-317",
                                                "(+)-Serum"))
         
         # Plot all treatments for treatment
-        g.all <- ggplot(dat.all, 
+        gAll <- ggplot(allDat, 
                         aes(x = interaction(TimePoint, Treatment, Target),
                             y = NormLog, 
                             fill = Target)) +
@@ -121,22 +143,22 @@ PlotTreatment <- function(control, treatment, targets, cellLine){
                 facet_grid(CellLine~.) +
                 labs(fill = "") + 
                 scale_x_discrete(labels = rep(c("0 h", "1 h", "24 h"), 
-                                              length(unique(dat.all$Target)))) +
+                                              length(unique(allDat$Target)))) +
                 xlab("Treatment Time") + 
                 ylab("Normalized Response") +
                 ggtitle(paste0("Treatment: ", treatment)) +
                 theme(axis.text.x = 
                               element_text(angle = 90, hjust = 1, vjust = 0.5))
         
-        ggsave(g.all, 
+        ggsave(gAll, 
                filename = paste0("Treatment Plots/", treatment, ".png"), 
                width = 12, height = 8)
         
         # Plot select treatments for treatment
-        dat.rx <- filter(dat.all, Target %in% targets & 
+        rxDat <- filter(allDat, Target %in% targets & 
                                  CellLine == cellLine)
         
-        txt <- ggplot(dat.rx, 
+        txt <- ggplot(rxDat, 
                       aes(x = interaction(TimePoint, Treatment, 
                                           Target, CellLine), 
                           y = NormLog, 
@@ -158,17 +180,17 @@ PlotTreatment <- function(control, treatment, targets, cellLine){
 }
 
 ## Pairwise treatment comparisons
-TreatmentComp <- function(treatments, targets, cellLine){
-        dat.rx <- filter(dat, Treatment == treatments & Target %in% targets &
+TreatmentComp <- function(data, treatments, targets, cellLine){
+        rxDat <- filter(data, Treatment == treatments & Target %in% targets &
                                  CellLine == cellLine)
-        print(head(dat.rx))
-        g <- ggplot(dat.rx, aes(x = Target, 
+        print(head(rxDat))
+        g <- ggplot(rxDat, aes(x = Target, 
                                 group = interaction(Treatment, 
                                                     TimePoint,
                                                     Target),
                                 y = NormLog))
         
-        fig_comp <- g + 
+        figComp <- g + 
                 geom_boxplot(aes(fill = interaction(Treatment, TimePoint))) + 
                 labs(fill = "") + 
                 xlab("Target") + 
@@ -177,7 +199,7 @@ TreatmentComp <- function(treatments, targets, cellLine){
                 theme(axis.text.x = 
                               element_text(angle = 45, hjust = 1))
         
-        fig_comp.2 <- g + 
+        figComp2 <- g + 
                 geom_boxplot(aes(fill = Treatment)) + 
                 facet_wrap(~TimePoint) + 
                 labs(fill = "") + 
@@ -188,23 +210,23 @@ TreatmentComp <- function(treatments, targets, cellLine){
         
         dir.create(path = "Treatment Comparisons", showWarnings = FALSE)
         
-        ggsave(fig_comp,
+        ggsave(figComp,
                filename = paste0("Treatment Comparisons/", treatments[1], "_", 
                                  treatments[2], "_", cellLine, ".png"), 
                width = 8, height = 6)
-        ggsave(fig_comp.2, 
+        ggsave(figComp2, 
                filename = paste0("Treatment Comparisons/", treatments[1], "_", 
                                  treatments[2], "_", cellLine, "_2.png"), 
                width = 8, height = 6)
 }
 
-Cytometry <- function(){
+Cytometry <- function(data){
         dir.create("Cytometry", showWarnings = FALSE)
-        dat.rx <- filter(dat, !grepl("Serum", Treatment))
-        pairList <- t(combn(unique(dat$Target), 2))
+        rxDat <- filter(data, !grepl("Serum", Treatment))
+        pairList <- t(combn(unique(data$Target), 2))
         for(i in seq_len(nrow(pairList))){
                 targets <- pairList[i, ]
-                cytDat <- filter(dat.rx, Target %in% pairList[i, ])
+                cytDat <- filter(rxDat, Target %in% pairList[i, ])
                 cytCast <- dcast(data = cytDat, Treatment + CellLine + 
                                          TimePoint + Replicate + n ~ Target,
                                  value.var = "NormLog")
@@ -230,43 +252,44 @@ PlotData <- function(){
         # Load libraries and set theme for all plots
         library(tidyverse)
         library(ggthemes)
+        library(biosensor)
         theme_set(theme_few(base_size = 16))
         
         # Load in data to make plots
-        setwd("D:/Box Sync/Data/")
-        dat <<- read_csv("compiledNormalized.csv")
+        x <- compLabelNorm()
+        compDat <- x[[1]]
         
         # Save current wd to return to later and setwd to plots folder
         directory <- getwd()
         setwd("../XPP_Plots/")
         
-        PlotAllData()
+        PlotAllData(data = compDat)
         
-        PlotEachTreatment()
+        PlotEachTreatment(data = compDat)
         
-        PlotEachTarget()
+        PlotEachTarget(data = compDat)
         
         control <- "DMSO"
-        txtList <- unique(dat$Treatment)
+        txtList <- unique(compDat$Treatment)
         compTargets <- c("pAktSer473", "pS6Ser235/6", "pS6Ser240/4",
                          "pp70S6KThr389", "pRbSer780", "pRbSer807/11")
         
         lapply(txtList, function(i){
-                PlotTreatment(control = control, treatment = i,
+                PlotTreatment(data = compDat, control = control, treatment = i,
                               targets = compTargets, cellLine = "GBM6")
-                PlotTreatment(control = control, treatment = i,
+                PlotTreatment(data = compDat, control = control, treatment = i,
                               targets = compTargets, cellLine = "GBM26")
         })
         
         # Pairwise treatment list
-        txtPairs <- combn(unique(dat$Treatment), 2, simplify = FALSE)
+        txtPairs <- combn(unique(compDat$Treatment), 2, simplify = FALSE)
         
         # Run through pair-wise list to plot treatment comparisons
         lapply(txtPairs, function(i){
-                TreatmentComp(treatments = as.vector(i), targets = compTargets,
-                              cellLine = "GBM6")
-                TreatmentComp(treatments = as.vector(i), targets = compTargets,
-                              cellLine = "GBM26")
+                TreatmentComp(data = compDat, treatments = as.vector(i),
+                              targets = compTargets, cellLine = "GBM6")
+                TreatmentComp(data = compDat, treatments = as.vector(i),
+                              targets = compTargets, cellLine = "GBM26")
         })
         setwd(directory)
 }
